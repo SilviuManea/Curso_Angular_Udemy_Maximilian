7a5f371e2ecfb0fa7f48736fc423814a0b78087a
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-05 16:27:40 +0200

    35-assignment Solution

--- assignments/basics-assignment-2-start/src/app/app.component.ts
+++ assignments/basics-assignment-2-start/src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  userName = "";
+
+  allowResetUser = false;
+
+  onResetUser() {
+    this.userName = "";
+    this.allowResetUser = false;
+  }
+
+  checkInput($event) {
+    console.log($event);
+    // if (this.userName.length > 0) {
+    //   this.allowResetUser = true;
+    // } else {
+    //   this.allowResetUser = false;
+    // }
+    this.allowResetUser = this.userName.length > 0;
+  }
 }
